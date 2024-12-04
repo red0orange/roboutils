2d1c6c4f091cb2549cd9038ff858ab6a738a3830
red0orange <huangdehao919@gmail.com>
2024-12-04 15:16:35 +0800

    update viser grasp, 添加 2D image 的交互功能

--- src/roboutils/vis/viser_grasp.py
+++ src/roboutils/vis/viser_grasp.py
@@ -1,11 +1,27 @@
 import time
 
+import cv2
+import imageio as iio
 import numpy as np
 import open3d as o3d
 import viser
 import viser.transforms as tf
 
 from roboutils.proj_llm_robot.pose_transform import update_pose
+
+
+def ray_intersection_with_xy_plane(origin, direction):
+    x0, y0, z0 = origin
+    dx, dy, dz = direction
+    
+    if dz == 0:
+        raise ValueError("The ray is parallel to the x-y plane and will not intersect.")
+    
+    t = -z0 / dz  # Calculate the parameter t for the intersection
+    x_intersect = x0 + t * dx
+    y_intersect = y0 + t * dy
+    
+    return (x_intersect, y_intersect, 0)
 
 
 class ViserForGrasp(object):
@@ -27,13 +43,87 @@ class ViserForGrasp(object):
             self.break_flag = True
             pass
 
+        self.gui_render_diffusion_steps = self.server.gui.add_button("Render Grasp Diffusion Steps")
+        self.diffusion_steps_data = None
+        self.results_images = None
+        @self.gui_render_diffusion_steps.on_click
+        def _render_diffusion_steps(event: viser.GuiEvent) -> None:
+            if self.diffusion_steps_data is not None:
+                client = event.client
+                client.scene.reset()
+
+                time_grasp_Ts = self.diffusion_steps_data['grasp_Ts']
+                obj_xyz  = self.diffusion_steps_data['obj_xyz']
+                images = []
+                for time_i in range(time_grasp_Ts.shape[0]):
+                    grasp_Ts = time_grasp_Ts[time_i]
+                    self.vis_grasp_scene(grasp_Ts, pc=obj_xyz, z_direction=True)
+                    time.sleep(0.03)
+                    images.append(client.get_render(height=480, width=640, transport_format="jpeg"))
+                    client.scene.reset()
+                self.results_images = images
+                # print("Render grasp diffusion steps to images")
+                # client.send_file_download(
+                #     "image.gif", iio.imwrite("<bytes>", images, extension="gif", loop=0)
+                # )
+                # print("Render grasp diffusion steps to image.gif")
+
         self.pc_index = 0
         self.mesh_index = 0
         self.coord_index = 0
         self.grasp_index = 0
         pass
 
+    def interact_image(self, image, name="image"):
+        image = cv2.flip(image, -1)
+        image_ori_width = image.shape[1]
+        image_ori_height = image.shape[0]
+        image_handle = self.add_image(image, name)
+
+        # 设置视角
+        self.server.scene.set_up_direction("+x")
+        @self.server.on_client_connect
+        def _(client: viser.ClientHandle) -> None:
+            # Set up the camera -- this gives a nice view of the full mesh.
+            client.camera.position = np.array([0.0, 0.0, -120.0])
+            client.camera.wxyz = np.array([0.0, 0.0, 0.0, 1.0])
+            camera_handle = client.camera
+            pass
+
+        # 点击事件
+        self.tmp_return_cross_point = None
+        @image_handle.on_click
+        def _image_click(event):
+            # print("Image click event:", event)
+            wxyz = event.target.wxyz
+            xyz = event.target.position
+            render_width = image_handle.render_width
+            render_height = image_handle.render_height
+
+            ray_origin = event.ray_origin
+            ray_direction = event.ray_direction
+
+            x_y_cross_point = ray_intersection_with_xy_plane(ray_origin, ray_direction)
+            print("x_y_cross_point:", x_y_cross_point)
+
+            width_ratio = image_ori_width / render_width
+            x_y_cross_point = (x_y_cross_point[0] * width_ratio, x_y_cross_point[1] * width_ratio)
+            x_y_cross_point = (int(x_y_cross_point[0]), int(x_y_cross_point[1]))
+
+            self.tmp_return_cross_point = np.array(x_y_cross_point)
+        
+        self.wait_for_reset()
+
+        return_cross_point = self.tmp_return_cross_point
+        return_cross_point[0] = -return_cross_point[0]
+        return_cross_point[1] = -return_cross_point[1]
+        return_cross_point[0] += (image_ori_width // 2)
+        return_cross_point[1] += (image_ori_height // 2)
+        image_handle.remove_click_callback()
+        return return_cross_point
+
     def wait_for_reset(self):
+        print("Waiting for reset...")
         while (not self.reset_flag) and (not self.break_flag) :
             time.sleep(0.1)
         self.server.scene.reset()
@@ -56,7 +146,7 @@ class ViserForGrasp(object):
         self.server.scene.add_mesh_simple(name=name, vertices=mesh_vertices, faces=mesh_faces)
         pass
 
-    def add_pcd(self, points, colors=None, name=None, point_size=0.003):
+    def add_pcd(self, points, colors=None, name=None, point_size=0.002):
         if name is None:
             name = "pc_{}".format(self.pc_index)
             self.pc_index += 1
@@ -83,7 +173,7 @@ class ViserForGrasp(object):
         self.server.scene.add_frame(name, wxyz=wxyz, position=xyz, axes_length=axes_length, axes_radius=axes_radius)
         pass
 
-    def add_grasp(self, grasp_T, name=None, z_direction=True):
+    def add_grasp(self, grasp_T, name=None, z_direction=True, grasp_color=None):
         if name is None:
             name = "grasp_{}".format(self.grasp_index)
             self.grasp_index += 1
@@ -93,6 +183,8 @@ class ViserForGrasp(object):
             grasp_T = update_pose(grasp_T, rotate=np.pi / 2, rotate_axis='x')
             pass
 
+        if grasp_color is None:
+            grasp_color = (90, 200, 255)
         meshes = self.get_gripper_control_points_o3d(grasp_T)
         for mesh_i, mesh in enumerate(meshes):
             sub_name = name + "/" + str(mesh_i)
@@ -100,12 +192,15 @@ class ViserForGrasp(object):
                 name=sub_name,
                 vertices=np.array(mesh.vertices),
                 faces=np.array(mesh.triangles),
+                color=grasp_color,
                 wxyz=tf.SO3.from_x_radians(0.0).wxyz,
                 position=(0.0, 0.0, 0.0),
             )
         pass
 
-    def vis_grasp_scene(self, grasp_Ts, pc=None, mesh=None, max_grasp_num=50, z_direction=True):
+    def vis_grasp_scene(self, grasp_Ts, pc=None, mesh=None, grasp_colors=None, max_grasp_num=50, z_direction=True):
+        if grasp_colors is None:
+            grasp_colors = [None] * len(grasp_Ts)
         if pc is not None:
             self.add_pcd(pc)
         if mesh is not None:
@@ -113,8 +208,23 @@ class ViserForGrasp(object):
         for grasp_i, grasp_T in enumerate(grasp_Ts):
             if grasp_i >= max_grasp_num:
                 break
-            self.add_grasp(grasp_T, z_direction=z_direction)
+            self.add_grasp(grasp_T, z_direction=z_direction, grasp_color=grasp_colors[grasp_i])
         pass
+
+    def add_image(self, image, name=None, fix_width=320):
+        if name is None:
+            name = "image"
+        render_ratio = fix_width / image.shape[1]
+        render_height = int(image.shape[0] * render_ratio)
+        image_handle = self.server.scene.add_image(name, image, render_width=fix_width, render_height=render_height)
+        return image_handle
+
+    def set_grasp_diffusion_steps(self, time_grasp_Ts, obj_xyz):
+        self.diffusion_steps_data = {'grasp_Ts': time_grasp_Ts, 'obj_xyz': obj_xyz}
+        pass
+
+    def get_grasp_diffusion_steps_images(self):
+        return self.results_images
 
     @staticmethod
     def get_gripper_control_points_o3d(grasp, color=(0.2, 0.8, 0)):
